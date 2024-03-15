2dd4bff79714bf1e005482b76a0daffc00865668
mortadhaksontini <mortadhaksontini22@gmail.com>
2024-03-15 03:07:05 +0000

    adding files

---
+++ src/redux/slices/app.js
@@ -0,0 +1,130 @@
+import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  snackbar: {
+    open: null,
+    severity: null,
+    message: null,
+  },
+  users: [],
+  friends: [],
+  friendRequests: [],
+  chat_type: null,
+  room_id: null,
+};
+
+const slice = createSlice({
+  name: "app",
+  initialState,
+  reducers: {
+    openSnackBar(state, action) {
+      console.log(action.payload);
+      state.snackbar.open = true;
+      state.snackbar.severity = action.payload.severity;
+      state.snackbar.message = action.payload.message;
+    },
+    closeSnackBar(state) {
+      console.log("This is getting executed");
+      state.snackbar.open = false;
+      state.snackbar.message = null;
+    },
+    updateUsers(state, action) {
+      state.users = action.payload.users;
+    },
+    updateFriends(state, action) {
+      state.friends = action.payload.friends;
+    },
+    updateFriendRequests(state, action) {
+      state.friendRequests = action.payload.requests;
+    },
+    selectConversation(state, action) {
+      state.chat_type = "individual";
+      state.room_id = action.payload.room_id;
+    },
+  },
+});
+export default slice.reducer;
+
+export const closeSnackBar = () => async (dispatch, getState) => {
+  dispatch(slice.actions.closeSnackBar());
+};
+
+export const showSnackbar =
+  ({ severity, message }) =>
+  async (dispatch, getState) => {
+    dispatch(
+      slice.actions.openSnackBar({
+        message,
+        severity,
+      })
+    );
+    setTimeout(() => {
+      dispatch(slice.actions.closeSnackBar());
+    }, 4000);
+  };
+
+export function FetchUsers() {
+  return async (dispatch, getState) => {
+    await axios
+      .get("/user/get-users", {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${getState().auth.token}`,
+        },
+      })
+      .then((response) => {
+        console.log(response);
+        dispatch(slice.actions.updateUsers({ users: response.data.data }));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+}
+
+export function FetchFriends() {
+  return async (dispatch, getState) => {
+    await axios
+      .get("/user/get-friends", {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${getState().auth.token}`,
+        },
+      })
+      .then((response) => {
+        console.log(response);
+        dispatch(slice.actions.updateFriends({ friends: response.data.data }));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+}
+
+export function FetchFriendRequests() {
+  return async (dispatch, getState) => {
+    await axios
+      .get("/user/get-friend-requests", {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${getState().auth.token}`,
+        },
+      })
+      .then((response) => {
+        console.log(response);
+        dispatch(
+          slice.actions.updateFriendRequests({ requests: response.data.data })
+        );
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+}
+
+export const SelectConversation = ({ room_id }) => {
+  return async (dispatch, getState) => {
+    dispatch(slice.actions.selectConversation({ room_id }));
+  };
+};
---
+++ src/sections/auth/ResetPasswordForm.js
@@ -0,0 +1,81 @@
+import React from "react";
+import * as Yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import FormProvider from "../../components/hook-form/FormProvider";
+import { Alert, Button, Stack } from "@mui/material";
+import { RHFTextField } from "../../components/hook-form";
+import { useDispatch } from "react-redux";
+import { ForgotPassword } from "../../redux/slices/auth";
+
+const ResetPasswordForm = () => {
+  const dispatch = useDispatch();
+  const ResetPasswordSchema = Yup.object().shape({
+    email: Yup.string()
+      .required("Email is required")
+      .email("Email must be a valid email address"),
+  });
+
+  const defaultValues = {
+    email: "demo@gmail.com",
+  };
+
+  const methods = useForm({
+    resolver: yupResolver(ResetPasswordSchema),
+    defaultValues,
+  });
+
+  const {
+    reset,
+    setError,
+    handleSubmit,
+    formState: { errors },
+  } = methods;
+
+  const onSubmit = (data) => {
+    try {
+      // submit data to backend
+      dispatch(ForgotPassword(data));
+    } catch (error) {
+      console.error(error);
+      reset();
+      setError("afterSubmit", {
+        ...error,
+        message: error.message,
+      });
+    }
+  };
+
+  return (
+    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+      <Stack spacing={3}>
+        {!!errors.afterSubmit && (
+          <Alert severity="error">{errors.afterSubmit.message}</Alert>
+        )}
+
+        <RHFTextField name="email" label="Email address" />
+        <Button
+          fullWidth
+          color="inherit"
+          size="large"
+          type="submit"
+          variant="contained"
+          sx={{
+            bgcolor: "text.primary",
+            color: (theme) =>
+              theme.palette.mode === "light" ? "common.white" : "grey.800",
+            "&:hover": {
+              bgcolor: "text.primary",
+              color: (theme) =>
+                theme.palette.mode === "light" ? "common.white" : "grey.800",
+            },
+          }}
+        >
+          Send Request
+        </Button>
+      </Stack>
+    </FormProvider>
+  );
+};
+
+export default ResetPasswordForm;
